2f7f3f888daf9db94b2c2c925ee6ced25ff46fb4
apurva kulkarni <apurva165@gmail.com>
2020-02-21 08:21:23 -0600

    first problem

---
+++ src/main/java/NegativeNumbersInMatrix_1351.java
@@ -0,0 +1,13 @@
+public class NegativeNumbersInMatrix_1351 {
+    public int countNegatives(int[][] grid) {
+        int count = 0;
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[i].length; j++) {
+                if (grid[i][j] < 0) {
+                    count = count + 1;
+                }
+            }
+        }
+        return count;
+    }
+}
